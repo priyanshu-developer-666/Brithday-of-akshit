f9eb896dd89eab8f8cf6a7b4dcdb8beb05552636
priyanshu-developer-666 <154962758+priyanshu-developer-666@users.noreply.github.com>
2024-02-15 19:13:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get the audio element
+var audio = document.getElementById("myAudio");
+
+// Play the audio when the page is loaded
+window.addEventListener("load", function() {
+  audio.play();
+});
